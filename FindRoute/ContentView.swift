34a67d7a40e1bfa73ad3e5a2d804271745118ce1
Artem <Serebryakov>
2022-08-24 16:13:00 +0300

    Initial Commit

---
+++ FindRoute/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FindRoute
+//
+//  Created by Artem Serebriakov on 24.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FindRoute/FindRouteApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindRouteApp.swift
+//  FindRoute
+//
+//  Created by Artem Serebriakov on 24.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct FindRouteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
