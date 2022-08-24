672f0ed7b174da2b3ef6d5f2209f1033b13a26cb
Artem <Serebryakov>
2022-08-24 17:53:45 +0300

    init

--- FindRoute/ContentView.swift
+++ FindRoute/ContentView.swift
@@ -6,11 +6,19 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            MapView()
+            Button(action: {
+                
+            }, label: {
+                Text("Show directions")
+            })
             .padding()
+        }
     }
 }
 
@@ -18,4 +26,64 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+var locationManager = CLLocationManager()
+
+struct MapView: UIViewRepresentable {
+    typealias UIViewType = MKMapView
+    
+    @Binding var directions: [String]
+    
+    func makeCoordinator() -> MapViewCoordinator {
+        return MapViewCoordinator()
+    }
+    
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        mapView.delegate = context.coordinator
+        
+//        if let location = locationManager.location?.coordinate {
+//            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: 40, longitudinalMeters: 40)
+//            mapView.setRegion(region, animated: true)
+//        }
+        let region = MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 40.71, longitude: -74),
+            span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+        mapView.setRegion(region, animated: true)
+        
+        let p1 = MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: 40.71, longitude: -74))
+        let p2 = MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: 42.46, longitude: -71.05))
+        
+        let request = MKDirections.Request()
+        request.source = MKMapItem(placemark: p1)
+        request.destination = MKMapItem(placemark: p2)
+        request.transportType  = .automobile
+        
+        let directions = MKDirections(request: request)
+        directions.calculate { response, error in
+            guard let route = response?.routes.first else { return }
+            mapView.addAnnotations([p1,p2])
+            mapView.addOverlay(route.polyline)
+            mapView.setVisibleMapRect(
+                route.polyline.boundingMapRect,
+                edgePadding: UIEdgeInsets(top: 40, left: 40, bottom: 40, right: 40),
+                animated: true
+            )
+        }
+        
+        return mapView
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {}
+    class MapViewCoordinator: NSObject, MKMapViewDelegate {
+        
+        func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+            let renderer = MKPolylineRenderer(overlay: overlay)
+            renderer.strokeColor = .systemBlue
+            renderer.lineWidth = 5
+            return renderer
+        }
+    }
+    
 }
