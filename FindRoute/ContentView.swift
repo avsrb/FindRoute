84816f555f66dbc18acc4315154a2586974cb03e
Artem <Serebryakov>
2022-08-25 11:33:35 +0300

    del pin as in the subject. I don't like them

--- FindRoute/ContentView.swift
+++ FindRoute/ContentView.swift
@@ -19,98 +19,100 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-    
-//    @State private var directions: [String] = []
-//    @State private var showDirections = false
-//
-//    var body: some View {
-//        VStack {
-//            MapView(directions: $directions)
-//
-//            Button(action: {
-//                self.showDirections.toggle()
-//            }, label: {
-//                Text("Show directions")
-//            })
-//            .disabled(directions.isEmpty)
-//            .padding()
-//        }.sheet(isPresented: $showDirections) {
-//            VStack {
-//                Text("Directions")
-//                    .font(.largeTitle)
-//                    .bold()
-//                    .padding()
-//
-//                Divider().background(Color.blue)
-//
-//                List {
-//                    ForEach(0..<self.directions.count, id: \.self) { i in
-//                        Text(self.directions[i])
-//                            .padding()
-//                    }
-//                }
-//            }
+
+/*
+    @State private var directions: [String] = []
+    @State private var showDirections = false
+
+    var body: some View {
+        VStack {
+            MapView(directions: $directions)
+
+            Button(action: {
+                self.showDirections.toggle()
+            }, label: {
+                Text("Show directions")
+            })
+            .disabled(directions.isEmpty)
+            .padding()
+        }.sheet(isPresented: $showDirections) {
+            VStack {
+                Text("Directions")
+                    .font(.largeTitle)
+                    .bold()
+                    .padding()
+
+                Divider().background(Color.blue)
+
+                List {
+                    ForEach(0..<self.directions.count, id: \.self) { i in
+                        Text(self.directions[i])
+                            .padding()
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+var locationManager = CLLocationManager()
+
+struct MapView: UIViewRepresentable {
+    typealias UIViewType = MKMapView
+
+    @Binding var directions: [String]
+    func makeCoordinator() -> MapViewCoordinator {
+        return MapViewCoordinator()
+    }
+
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        mapView.delegate = context.coordinator
+
+//        if let location = locationManager.location?.coordinate {
+//            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: 40, longitudinalMeters: 40)
+//            mapView.setRegion(region, animated: true)
 //        }
-//    }
-//}
-//
-//
-//var locationManager = CLLocationManager()
-//
-//struct MapView: UIViewRepresentable {
-//    typealias UIViewType = MKMapView
-//
-//    @Binding var directions: [String]
-//    func makeCoordinator() -> MapViewCoordinator {
-//        return MapViewCoordinator()
-//    }
-//
-//    func makeUIView(context: Context) -> MKMapView {
-//        let mapView = MKMapView()
-//        mapView.delegate = context.coordinator
-//
-////        if let location = locationManager.location?.coordinate {
-////            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: 40, longitudinalMeters: 40)
-////            mapView.setRegion(region, animated: true)
-////        }
-//        let region = MKCoordinateRegion(
-//            center: CLLocationCoordinate2D(latitude: 40.71, longitude: -74),
-//            span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
-//        mapView.setRegion(region, animated: true)
-//
-//        let p1 = MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: 40.71, longitude: -74))
-//        let p2 = MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: 42.46, longitude: -71.05))
-//
-//        let request = MKDirections.Request()
-//        request.source = MKMapItem(placemark: p1)
-//        request.destination = MKMapItem(placemark: p2)
-//        request.transportType  = .automobile
-//
-//        let directions = MKDirections(request: request)
-//        directions.calculate { response, error in
-//            guard let route = response?.routes.first else { return }
-//            mapView.addAnnotations([p1,p2])
-//            mapView.addOverlay(route.polyline)
-//            mapView.setVisibleMapRect(
-//                route.polyline.boundingMapRect,
-//                edgePadding: UIEdgeInsets(top: 40, left: 40, bottom: 40, right: 40),
-//                animated: true
-//            )
-//            self.directions = route.steps.map { $0.instructions }.filter{ !$0.isEmpty }
-//        }
-//
-//        return mapView
-//    }
-//
-//    func updateUIView(_ uiView: MKMapView, context: Context) {}
-//    class MapViewCoordinator: NSObject, MKMapViewDelegate {
-//
-//        func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
-//            let renderer = MKPolylineRenderer(overlay: overlay)
-//            renderer.strokeColor = .systemBlue
-//            renderer.lineWidth = 5
-//            return renderer
-//        }
-//    }
-//
-//}
+        let region = MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 40.71, longitude: -74),
+            span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+        mapView.setRegion(region, animated: true)
+
+        let p1 = MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: 40.71, longitude: -74))
+        let p2 = MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: 42.46, longitude: -71.05))
+
+        let request = MKDirections.Request()
+        request.source = MKMapItem(placemark: p1)
+        request.destination = MKMapItem(placemark: p2)
+        request.transportType  = .automobile
+
+        let directions = MKDirections(request: request)
+        directions.calculate { response, error in
+            guard let route = response?.routes.first else { return }
+            mapView.addAnnotations([p1,p2])
+            mapView.addOverlay(route.polyline)
+            mapView.setVisibleMapRect(
+                route.polyline.boundingMapRect,
+                edgePadding: UIEdgeInsets(top: 40, left: 40, bottom: 40, right: 40),
+                animated: true
+            )
+            self.directions = route.steps.map { $0.instructions }.filter{ !$0.isEmpty }
+        }
+
+        return mapView
+    }
+
+    func updateUIView(_ uiView: MKMapView, context: Context) {}
+    class MapViewCoordinator: NSObject, MKMapViewDelegate {
+
+        func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+            let renderer = MKPolylineRenderer(overlay: overlay)
+            renderer.strokeColor = .systemBlue
+            renderer.lineWidth = 5
+            return renderer
+        }
+    }
+
+}
+*/
--- FindRoute/View/MapView.swift
+++ FindRoute/View/MapView.swift
@@ -17,29 +17,66 @@ struct MapView: UIViewRepresentable {
     }
     
     func makeUIView(context: Context) -> MKMapView {
-        let view = mapData.mapView
+        let mapView = mapData.mapView
         
-        view.showsUserLocation = true
-        view.delegate = context.coordinator
-        return view 
+        mapView.showsUserLocation = true
+        mapView.delegate = context.coordinator
+        
+        let region = MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 40.71, longitude: -74),
+            span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+        mapView.setRegion(region, animated: true)
+
+        let p1 = MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: 40.71, longitude: -74))
+        let p2 = MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: 42.46, longitude: -71.05))
+
+        let request = MKDirections.Request()
+        request.source = MKMapItem(placemark: p1)
+        request.destination = MKMapItem(placemark: p2)
+        request.transportType  = .automobile
+
+        let directions = MKDirections(request: request)
+        directions.calculate { response, error in
+            guard let route = response?.routes.first else { return }
+            mapView.addAnnotations([p1,p2])
+            mapView.addOverlay(route.polyline)
+            mapView.setVisibleMapRect(
+                route.polyline.boundingMapRect,
+                edgePadding: UIEdgeInsets(top: 40, left: 40, bottom: 40, right: 40),
+                animated: true
+            )
+        }
+        
+        return mapView
     }
     
     func updateUIView(_ uiView: MKMapView, context: Context) {
         
     }
+
+    class MapViewCoordinator: NSObject, MKMapViewDelegate {
+
+        func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+            let renderer = MKPolylineRenderer(overlay: overlay)
+            renderer.strokeColor = .systemBlue
+            renderer.lineWidth = 5
+            return renderer
+        }
+    }
     
     class Coordinator: NSObject, MKMapViewDelegate {
-        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-            
-            if annotation.isKind(of: MKUserLocation.self) { return nil }
-            else {
-                let pinAnnotation = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "PIN_VIEW")
-                pinAnnotation.tintColor = .green
-                pinAnnotation.animatesDrop = true
-                pinAnnotation.canShowCallout = true
-                
-                return pinAnnotation
-            }
-        }
+        // MARK: Pin as in the subject
+//        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+//
+//            if annotation.isKind(of: MKUserLocation.self) { return nil }
+//            else {
+//                let pinAnnotation = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "PIN_VIEW")
+//                pinAnnotation.tintColor = .green
+//                pinAnnotation.animatesDrop = true
+//                pinAnnotation.canShowCallout = true
+//
+//                return pinAnnotation
+//            }
+//        }
     }
 }
